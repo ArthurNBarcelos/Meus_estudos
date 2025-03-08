1db73eb82189167308ffce0a9e3d66eaaca96b5f
Arthur N. Barcelos <arthurneutzlingbarcelos@gmail.com>
2025-03-08 18:37:44 -0300

    Create exercicios.py

---
+++ Estudos/Python (Unisinos)/Programação I (semana 1 e 2)/exercicios.py
@@ -0,0 +1,34 @@
+"Exercício 5"
+print("Hello World!")
+
+"Exercício 6"
+x = 3+5*7
+print(x)
+
+"Exercício 7"
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Peso: ", peso)
+
+"Exercício 8"
+"Parte 1"
+v1 = float(input("Digite o primeiro valor: "))
+v2 = float(input("Digite o segundo valor: "))
+resultado = v1/v2
+resultadoFormatado = float("%.2f" % resultado)
+print("Resultado: ", resultadoFormatado)
+"Parte 2"
+v1 = float(input("Digite o primeiro valor: "))
+v2 = float(input("Digite o segundo valor: "))
+resultado = v1/v2
+resultadoFormatado = float("%.3f" % resultado)
+print("Resultado: ", resultadoFormatado)
+"Parte 3"
+v1 = float(input("Digite o primeiro valor: "))
+v2 = float(input("Digite o segundo valor: "))
+resultado = v1/v2
+resultadoFormatado = float("%.4f" % resultado)
+print("Resultado: ", resultadoFormatado)
